2e1f7ead5a401c5d845cfbef1d7f4fdb366e6b47
GutnykYaroslav <c.hutnyk.yaroslav@student.uzhnu.edu.ua>
2026-02-13 18:43:03 +0200

    one

--- Lab-6/Program.cs
+++ Lab-6/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿abstract class Vehicle
+{
+    public string brand;
+    public int speed;
+
+    public abstract void Move();
+}
+
+class Bicycle : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Велосипед їде");
+    }
+}
+
+class Airplane : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Літак літає");
+    }
+
+    public void Refill()
+    {
+        Console.WriteLine("Літак заправляється");
+    }
+}
+
+class Car : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Машина їде");
+    }
+
+    public void Refill()
+    {
+        Console.WriteLine("Машина заправляється");
+    }
+}
