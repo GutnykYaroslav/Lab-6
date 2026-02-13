8a1f71f0779f265eeda4f63d260a4015fb8a11e5
GutnykYaroslav <c.hutnyk.yaroslav@student.uzhnu.edu.ua>
2026-02-13 19:47:50 +0200

    three

--- Lab-6/Program.cs
+++ Lab-6/Program.cs
@@ -1,70 +1,61 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
-abstract class Vehicle
+public abstract class Animal
 {
-    public string brand;
-    public int speed;
+    public string Name { get; set; }
 
-    public abstract void Move();
-}
-
-class Bicycle : Vehicle
-{
-    public override void Move()
+    public Animal(string name)
     {
-        Console.WriteLine("Велосипед їде");
-    }
-}
-
-class Airplane : Vehicle
-{
-    public override void Move()
-    {
-        Console.WriteLine("Літак злітає");
+        Name = name;
     }
 
-    public void Refill()
+    public abstract void MakeSound();
+}
+
+public class Dog : Animal
+{
+    public Dog(string name) : base(name) {}
+
+    public override void MakeSound()
     {
-        Console.WriteLine("Літак літає");
+        Console.WriteLine($"{Name} каже: Гав-гав!");
     }
 }
 
-class Car : Vehicle
+public class Cat : Animal
 {
-    public override void Move()
-    {
-        Console.WriteLine("Машина їде");
-    }
+    public Cat(string name) : base(name) {}
 
-    public void Refill()
+    public override void MakeSound()
     {
-        Console.WriteLine("Машина зупиняється");
+        Console.WriteLine($"{Name} каже: Мяу-мяу!");
     }
 }
+
+public class Parrot : Animal
+{
+    public Parrot(string name) : base(name) {}
+
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} каже: Привіт! Піастри!");
+    }
+}
+
 class Program
 {
-    public static void Main()
+    static void Main( string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.UTF8;
 
-        List<Vehicle> vehicles = new List<Vehicle> { new Car(), new Bicycle(), new Airplane() };
+        Animal myDog = new Dog("Рекс");
+        Animal myCat = new Cat("Мурчик");
+        Animal myParrot = new Parrot("Кеша");
 
-        foreach (Vehicle vehicle in vehicles)
-        {
-            vehicle.Move();
-
-            if (vehicle is Car car)
-            {
-                car.Refill();
-            }
-
-            if (vehicle is Airplane airplane)
-            {
-                airplane.Refill();
-            }
-        }
-
-        Console.WriteLine("Протестувати новий коміт");
+        myDog.MakeSound();
+        myCat.MakeSound();
+        myParrot.MakeSound();
     }
 }
